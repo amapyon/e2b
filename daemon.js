045e9611c7c1c384a96e5d9b696cdbdc18e91a64
AMANO Masaru <github@amapyon.org>
2017-03-25 21:43:09 +0900

    修正

--- daemon.js
+++ daemon.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const PID_FILE = '/var/run/e2b.pid';
+const PID_FILE = '/var/run/e2b_panel.pid';
 
 var p = require('process');
 var f = require('fs');
-var e2b = require('./e2b.js').E2B;
+var e2b = require('./e2b_panel.js').E2B;
 
 var cmd = p.argv[2];
 if (cmd == 'start') {
--- e2b_panel.sh
+++ e2b_panel.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-cd /home/masaru/pj/node/e2b
-exec /home/masaru/pj/node/nvm/versions/node/v4.3.1/bin/npm $1
+APP_HOME='/home/masaru/pj/node/e2b_panel'
+NPM='/home/masaru/pj/node/nvm/versions/node/v4.3.1/bin/npm'
+
+cd $APP_HOME
+exec $NPM $1
